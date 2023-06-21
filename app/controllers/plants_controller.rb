837c7bda661da835c4393b7b3b904fdc3a024797
brianrono <brianrono16@gmail.com>
2023-06-21 12:03:42 +0300

    Complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   # GET /plants
   def index
     plants = Plant.all
@@ -9,13 +8,46 @@ class PlantsController < ApplicationController
   # GET /plants/:id
   def show
     plant = Plant.find_by(id: params[:id])
-    render json: plant
+    if plant
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      if plant.update(plant_params)
+        render json: plant
+      else
+        render json: { error: "Failed to update plant" }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      render json: { message: "Plant deleted successfully" }
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
   end
 
   # POST /plants
   def create
     plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    if plant.valid?
+      render json: plant, status: :created
+    else
+      render json: { error: plant.errors.full_messages.join(", ") }, status: :unprocessable_entity
+    end
   end
 
   private
